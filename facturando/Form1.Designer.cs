6762d377b8594021066f11100b474b731d2bc752
Steven Vasquez <stevenvasquez652@gmail.com>
2023-09-27 21:05:02 -0400

    Este es el codigo del profe copiado desde el video de yotube

---
+++ facturando/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace facturando
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnguardar = new Button();
+            dataGridView1 = new DataGridView();
+            item = new DataGridViewTextBoxColumn();
+            cantidad = new DataGridViewTextBoxColumn();
+            descripcion = new DataGridViewTextBoxColumn();
+            PU = new DataGridViewTextBoxColumn();
+            Total = new DataGridViewTextBoxColumn();
+            dataGridView2 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // btnguardar
+            // 
+            btnguardar.Location = new Point(657, 432);
+            btnguardar.Name = "btnguardar";
+            btnguardar.Size = new Size(75, 23);
+            btnguardar.TabIndex = 0;
+            btnguardar.Text = "Guardar";
+            btnguardar.UseVisualStyleBackColor = true;
+            btnguardar.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { item, cantidad, descripcion, PU, Total });
+            dataGridView1.Location = new Point(22, 25);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(583, 150);
+            dataGridView1.TabIndex = 1;
+            // 
+            // item
+            // 
+            item.HeaderText = "item";
+            item.Name = "item";
+            // 
+            // cantidad
+            // 
+            cantidad.HeaderText = "cantidad";
+            cantidad.Name = "cantidad";
+            // 
+            // descripcion
+            // 
+            descripcion.HeaderText = "descripcion";
+            descripcion.Name = "descripcion";
+            // 
+            // PU
+            // 
+            PU.HeaderText = "PU";
+            PU.Name = "PU";
+            // 
+            // Total
+            // 
+            Total.HeaderText = "Total";
+            Total.Name = "Total";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(22, 229);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(583, 150);
+            dataGridView2.TabIndex = 2;
+            dataGridView2.CellEndEdit += dataGridView2_CellEndEdit;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(839, 539);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnguardar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnguardar;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn item;
+        private DataGridViewTextBoxColumn cantidad;
+        private DataGridViewTextBoxColumn descripcion;
+        private DataGridViewTextBoxColumn PU;
+        private DataGridViewTextBoxColumn Total;
+        private DataGridView dataGridView2;
+    }
+}
---
+++ facturando/Form1.cs
@@ -0,0 +1,94 @@
+using Microsoft.Data.SqlClient;
+using System;
+//using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace facturando
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conexion = new SqlConnection("Data Source =.;Initial Catalog = facturando;" + "Integrated Security=true;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //falta arreglar la parte del total en el table gridview 1 , preguntar al profe a ver mas o menos como es
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            verdatos();
+        }
+
+        private void verdatos()
+        {
+            try
+            {
+
+                conexion.Open();
+                string consulta = ("select item,cantidad,descripcion,PU, PU*cantidad as total  from factura");
+                SqlCommand comando = new SqlCommand(consulta, conexion);
+                SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet, "factura");
+                dataGridView2.DataSource = dataSet.Tables["factura"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            finally { conexion.Close(); }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+
+                    if (!row.IsNewRow && !String.IsNullOrEmpty(row.Cells[0].Value.ToString()))
+                    {
+                        int item = int.Parse(row.Cells[0].Value.ToString());
+                        int cantidad = int.Parse(row.Cells[1].Value.ToString());
+                        string descripcion = row.Cells[2].Value.ToString();
+                        double PU = double.Parse(row.Cells[0].Value.ToString());
+
+                        string consulta = "INSERT INTO factura(item,cantidad,descripcion,PU) VALUES (@item, @cantidad, @descripcion, @PU)";
+                        SqlCommand comando = new SqlCommand(consulta, conexion);
+
+                        comando.Parameters.AddWithValue("@item", item);
+                        comando.Parameters.AddWithValue("@cantidad", cantidad);
+                        comando.Parameters.AddWithValue("@descripcion", descripcion);
+                        comando.Parameters.AddWithValue("@PU", PU);
+                        comando.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Guardado");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("ERROR AL GUARDAR LOS DATOS: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+                verdatos();
+            }
+        }
+
+        private void dataGridView2_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = ((DataGridViewRow)(dataGridView1.Rows[e.RowIndex]));
+            double cantidad = Convert.ToDouble(row.Cells[1].Value);
+            double PU = Convert.ToDouble(row.Cells[3].Value);
+            row.Cells[4].Value = (cantidad * PU);
+
+        }
+    }
+}
